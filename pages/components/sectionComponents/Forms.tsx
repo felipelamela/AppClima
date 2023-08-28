129ff0266340790eab508a0fe7d88a2b8877dce5
Felipe <fe-fl@outlook.com>
2023-08-28 00:23:27 -0300

    [Forms] Editado o forms com validação e função cleanInputs para limpar os inputs

--- pages/components/sectionComponents/Forms.tsx
+++ pages/components/sectionComponents/Forms.tsx
@@ -1,23 +1,50 @@
+'use client'
 import React from 'react'
 import Input from '../globalComponents/Input'
 import Button from '../globalComponents/Button'
 import TextArea from '../globalComponents/TextArea'
+import cleanInputs from '../utilits/cleanInputs'
+import InputFile from '../globalComponents/InputFile'
 
 const Forms = () => {
+  const [nome, setNome] = React.useState<string>('')
+  const [email, setEmail] = React.useState<string>('')
+  const [contato, setContato] = React.useState<string>('')
+  const [mensagem, setMensagem] = React.useState<string>('')
+  const [arquivo, setArquivo] = React.useState<File | null>(null)
 
-
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): null {
     e.preventDefault()
+    const novoContato = {
+      'nome': nome,
+      'email': email,
+      'contato': contato,
+      'mensagem': mensagem,
+      'pdf': arquivo
+    }
+    if (!arquivo === null) {
+
+      console.log(novoContato)
+      //Limpando os inputs após enviar
+      cleanInputs(
+        setNome,
+        setEmail,
+        setContato,
+        setMensagem,
+        setArquivo)
+    }
+    return null
   }
 
   return (
     <>
       <form onSubmit={e => handleSubmit(e)}>
 
-        <Input label='Nome' type='text' />
-        <Input label='Email' type='text' />
-        <Input label='Contato' type='tel' />
-        <TextArea />
+        <Input label='Nome' type='text' valor={nome} setValor={setNome} />
+        <Input label='Email' type='text' valor={email} setValor={setEmail} />
+        <Input label='Contato' type='tel' valor={contato} setValor={setContato} />
+        <TextArea name='Mensagem' valor={mensagem} setValor={setMensagem} />
+        <InputFile label='Arquivo' tipoArquivo='.pdf' setValor={setArquivo} />
         <Button name='Enviar' />
       </form>
       <style jsx>{`
