fc8a9e198145e336ea7ddb7560d4db375b4b1bbb
Felipe <fe-fl@outlook.com>
2023-08-28 00:23:52 -0300

    [TextArea] Component TextArea

--- pages/components/globalComponents/TextArea.tsx
+++ pages/components/globalComponents/TextArea.tsx
@@ -1,10 +1,24 @@
+'use client'
 import React from 'react'
+import { TextAreaTypes } from '../../../types'
 
-const TextArea = () => {
+const TextArea: React.FC<TextAreaTypes> = ({ name, valor, setValor }) => {
   return (
     <>
-      <label htmlFor=""></label>
-      <textarea>TextArea</textarea>
+      <label htmlFor={name}>{name}</label>
+      <textarea id={name} value={valor} onChange={e => setValor(e.target.value)} />
+      <style jsx>{`
+        textarea{
+          max-width: 600px
+          width: 100%;
+          height: 150px; 
+          resize: none;
+          margin: .3rem 0 0;
+          font-size: 1.2rem;
+          padding: .4rem ;
+          font-weight: regular;
+        }
+      `}</style>
     </>
   )
 }
