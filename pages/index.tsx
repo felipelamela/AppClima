1e84706499f789e7290dd86f917248af4c8548e2
Felipe <fe-fl@outlook.com>
2023-08-29 12:07:53 -0300

    [Index page] -> Separado o useEffect em dois modos, primeiro que vai receber os dados da localização do usuario, segundo fecht que faz a busca na API SEM TRATAMENTO AINDA

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 const index = () => {
   const [latUser, setLatUser] = React.useState<number | null>(null)
@@ -6,19 +7,33 @@ const index = () => {
 
 
   React.useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        console.log("Latitude:", latitude, "Longitude:", longitude);
-        setLatUser(latitude)
-        setLngUser(longitude)
-      });
-    }
-
-
-
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log("Latitude:", latitude, "Longitude:", longitude);
+      setLatUser(latitude)
+      setLngUser(longitude)
+    });
   }, [])
+
+  React.useEffect(() => {
+    if (latUser !== null) {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latUser}&lon=${lngUser}&units=metric&appid=831abf463c39305a62a4a3ec257719a7`)
+        .then(response => {
+          const endereco = response.data;
+          console.log(endereco)
+          if (!endereco.erro) {
+            console.log('CEP encontrado:', endereco.cep);
+          } else {
+            console.log('Rua não encontrada.');
+          }
+        })
+        .catch(error => {
+          console.error('Erro ao consultar o CEP:', error);
+        });
+    }
+  }, [latUser])
+
 
 
   return (
@@ -33,3 +48,35 @@ const index = () => {
 export default index
 
 // chave api https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
+
+
+/*
+
+
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log("Latitude:", latitude, "Longitude:", longitude);
+    setLatUser(latitude)
+    setLngUser(longitude)
+  });
+}
+
+if (latUser && lngUser) {
+  axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latUser}&lon=${lngUser}&appid=831abf463c39305a62a4a3ec257719a7`)
+    .then(response => {
+      const endereco = response.data;
+      console.log(endereco)
+      if (!endereco.erro) {
+        console.log('CEP encontrado:', endereco.cep);
+      } else {
+        console.log('Rua não encontrada.');
+      }
+    })
+    .catch(error => {
+      console.error('Erro ao consultar o CEP:', error);
+    });
+}
+
+*/
